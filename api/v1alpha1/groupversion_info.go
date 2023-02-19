7bc997ceb5b6f67f720f3ccdb6e7e7c2bfa2e691
LittleChimera <luka.skugor@chimeramail.com>
2023-02-19 16:19:58 +0100

    Enable server-side apply

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-//+kubebuilder:object:generate=true
+// +kubebuilder:object:generate=true
 package v1alpha1
 
 import (
